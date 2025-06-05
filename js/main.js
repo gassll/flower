fd98ec3c21784fb6d820e0dbdd72e16c7c60c7f7
gassll <lizakonopatskaya@gmail.com>
2025-06-05 14:53:52 +0300

    главную переделала

--- js/main.js
+++ js/main.js
@@ -124,3 +124,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
     textElements.forEach(el => observer.observe(el));
 });
+
+
+
+// const modal = document.getElementById("myModal");
+// const btn = document.getElementById("openModal");
+// const span = document.getElementById("closeModal");
+
+// btn.onclick = function () {
+//     modal.style.display = "block";
+// }
+
+// span.onclick = function () {
+//     modal.style.display = "none";
+// }
+
+// window.onclick = function (event) {
+//     if (event.target === modal) {
+//         modal.style.display = "none";
+//     }
+// }
